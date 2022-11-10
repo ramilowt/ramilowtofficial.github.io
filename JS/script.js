57fc623d72df2358eed18f95b4a57c7ad4aa7c24
ramilowt <ramilowt@students.nu-fairview.edu.ph>
2022-11-10 15:13:08 +0800

    Added new files

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing", {
+    strings:["","Information Technology(IT) Student","Game Programmer","Game Tester","Game Designer"],
+    typedSpeed:150,
+    BackSpeed:150,
+    loop:true
+})
